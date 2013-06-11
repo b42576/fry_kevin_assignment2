ee8dc2f9b927d54bfcdadbb1e47d7c4331d88cc0
Kevin Fry <kevinfry@fullsail.edu>
2013-06-11 16:40:43 -0400

    published files to github

---
+++ fry_kevin_assignment2.js
@@ -0,0 +1,28 @@
+// Kevin Fry
+// SDI 1306
+// Project 2
+// functions and more
+
+var varString = "";
+var numMaxTweetCharacters = 160;
+var numMyTweetCharacters = 150;
+var arrSocialNetworks = "";
+var outcome;
+
+var funTweetCharacters = function(myTweetChars,myMaxTweetChars){
+    var myoutcome;
+    myoutcome = myMaxTweetChars - myTweetChars;
+};
+
+//call functions
+//store returned values
+//procedure
+
+//boolean function 2 arguments
+
+//number function
+
+//string function
+
+//array function
+
